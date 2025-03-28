a8193b5851955a0eaaa202aa407e97f95e19ac99
zobirofkir <zobirofkir19@gmail.com>
2025-03-28 01:25:53 +0000

    create login and register pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+       {/**
+        * Home Route
+        */}
+      <Route path="/" element={<Home />} />
+      {/**
+       * Authenticated Routes
+       */}
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
+      
       </Routes>
     </BrowserRouter>
   );
