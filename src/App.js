a6a60b0463c5f9b7be1793f1dd37128359e1c5f4
zobirofkir <zobirofkir19@gmail.com>
2025-03-31 01:52:11 +0000

    add prefix auth from app.js

--- src/App.js
+++ src/App.js
@@ -1,77 +1,80 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Dashboard from './pages/dashboard/Dashboard';
-import ProtectedRoute from './protected/protected-route';
-import { Toaster } from 'react-hot-toast';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import Profile from './pages/auth/Profile';
-import Theme from './pages/store/Theme';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Dashboard from "./pages/dashboard/Dashboard";
+import ProtectedRoute from "./protected/protected-route";
+import { Toaster } from "react-hot-toast";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import Profile from "./pages/auth/Profile";
+import Theme from "./pages/store/Theme";
 
 function App() {
   return (
     <BrowserRouter>
+
       {/* Toast notifications */}
-      <Toaster 
+      <Toaster
         position="top-right"
         toastOptions={{
           duration: 4000,
           style: {
-            background: '#333',
-            color: '#fff',
+            background: "#333",
+            color: "#fff",
           },
           success: {
             style: {
-              background: 'green',
+              background: "green",
             },
           },
           error: {
             style: {
-              background: 'red',
+              background: "red",
             },
           },
         }}
       />
-      
+
       <Routes>
-        {/* Home Route */}
+
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
 
-        {/* Authenticated Routes */}
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/auth/forgot-password" element={<ForgotPassword />} />
+        {/* Auth Routes With Prefix /auth*/}
+        <Route path="/auth">
         
-        {/* Protected Dashboard Routes */}
-        <Route 
-          path="/auth/dashboard" 
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path='/auth/profile'
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }  
-        />
-        
-        {/* Themes Routes */}
-        <Route
-          path="/auth/themes"
-          element={
-            <ProtectedRoute>
-              <Theme />
-            </ProtectedRoute>
-          }
-        />
+          {/* Auth Pages */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
 
+          {/* Protected Routes */}
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="themes"
+            element={
+              <ProtectedRoute>
+                <Theme />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
