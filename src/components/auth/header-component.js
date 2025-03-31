2563e711c92ced61ec10c62d0e7b3821f8bf3892
zobirofkir <zobirofkir19@gmail.com>
2025-03-31 01:48:56 +0000

    add live button

--- src/components/auth/header-component.js
+++ src/components/auth/header-component.js
@@ -84,7 +84,30 @@ const HeaderComponent = ({ darkMode, setDarkMode }) => {
           AI
         </h1>
       </Link>
+      
       <div className="flex items-center space-x-4">
+      
+      {/* View Live Store */}
+      {currentUser && currentUser.domain && (
+            <a
+            href={`http://${currentUser.domain}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-4 md:mt-0 md:flex hidden items-center px-4 py-2 bg-purple-600 hover:bg-purple-700 text-white rounded-lg transition-colors shadow-md"
+            >
+            <span className="mr-2">View Live Store</span>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+            >
+                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+            </svg>
+            </a>
+        )}
+
         <button
           onClick={toggleDarkMode}
           className="p-2 rounded-lg bg-purple-100 dark:bg-purple-900 text-purple-800 dark:text-purple-200 flex items-center transition-colors duration-300"
